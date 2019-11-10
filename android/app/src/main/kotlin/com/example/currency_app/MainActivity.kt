07068ca60a3911afa9549edfcbb653e26192f102
egbred <default51rus@gmail.com>
2019-11-10 06:28:19 +0300

    Clean-up

--- android/app/src/main/kotlin/com/example/currency_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/currency_app/MainActivity.kt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.currency_app
 
 import android.os.Bundle
@@ -12,18 +11,3 @@ class MainActivity: FlutterActivity() {
     GeneratedPluginRegistrant.registerWith(this)
   }
 }
-=======
-package com.example.currency_app
-
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
-}
->>>>>>> e2aaba27176aca35e0c389d10d37bbea8f125111
